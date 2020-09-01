b34776012babf333ff6fd7334e617a1d8794e7c6
Amjad Saadeh <mail@amjadsaadeh.de>
2020-09-01 16:08:05 +0200

    make login possible

--- webapp/main.py
+++ webapp/main.py
@@ -4,15 +4,21 @@ import json
 import logging
 import datetime
 
-from flask import Flask, request, session, render_template, url_for, redirect
+from flask import Flask, request, session, render_template, url_for, redirect, flash
 from authlib.integrations.flask_client import OAuth
 
-from google.cloud import firestore
+from google.cloud import firestore, storage
+import google.auth.credentials
+
+
+BUCKET_NAME = 'kita-menu'
+
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
 oauth = OAuth(app)
 
+db = firestore.Client()
 
 oauth.register(
     name='amazon',
@@ -32,7 +38,12 @@ def index():
     if 'user_id' not in session:
         return redirect('login')
     
-    return render_template('index.html')
+    doc_ref = db.collection(u'menus').document(session['user_id'])
+    doc = doc_ref.get().to_dict()
+    if doc is None:
+        doc_ref.create({'cw': datetime.datetime.now().isocalendar()[1], 'menu': {}})
+
+    return render_template('index.html', menu=doc['menu'])
 
 @app.route('/login', methods=['GET'])
 def login():
@@ -56,8 +67,49 @@ def amazon_auth_finished():
 @app.route('/logout', methods=['GET'])
 def logout():
     session.pop('user_id', None)
-    session.pop('name')
+    session.pop('name', None)
     return redirect(url_for('login'))
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[-1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if 'user_id' not in session:
+        return redirect('login')
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            file_ext = file.filename.rsplit('.', 1)[-1].lower()
+            file.save('tmp.{:s}'.format(file_ext))
+            client = storage.Client()
+            bucket = client.bucket(BUCKET_NAME)
+            blob = bucket.blob('{:s}.{:s}'.format(session['user_id'], file_ext))
+            blob.upload_from_filename('tmp.{:s}'.format(file_ext))
+            return redirect('upload')
+    return '''
+            <!doctype html>
+            <title>Upload new File</title>
+            <h1>Upload new File</h1>
+            <form method=post enctype=multipart/form-data>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+            </form>
+            '''
+    
+
 
 if __name__ == '__main__':
     PORT = int(os.getenv('PORT')) if os.getenv('PORT') else 8080
